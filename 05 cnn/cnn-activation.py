b1057f0aa4d723218cc2d2cd8a1b568641e49de9
Easton Hou <shho@microsoft.com>
2017-10-13 15:01:59 +0800

    。

--- 05 cnn/cnn-activation.py
+++ 05 cnn/cnn-activation.py
@@ -1,4 +1,4 @@
-﻿from collections import defaultdict
+from collections import defaultdict
 from collections import Counter
 import numpy as np
 import sys
@@ -11,37 +11,31 @@ tf.app.flags.DEFINE_string("input_dir", "input", "Input directory where training
 tf.app.flags.DEFINE_string("output_dir", "output", "Output directory where output such as logs are saved.")
 tf.app.flags.DEFINE_string("model_dir", "output", "Model directory where final model files are saved.")
 
-# Define the model
-EMB_SIZE = 64
-WIN_SIZE = 3
-FILTER_SIZE = 64
+np.set_printoptions(linewidth=np.nan, threshold=np.nan)
 
 # Functions to read in the corpus
 w2i = defaultdict(lambda: len(w2i))
-t2i = defaultdict(lambda: len(t2i))
 UNK = w2i["<unk>"]
 def read_dataset(filename):
     with open(filename, "r") as f:
         for line in f:
             tag, words = line.lower().strip().split(" ||| ")
-            yield ([w2i[x] for x in words.split(" ")], t2i[tag])
+            words = words.split(" ")
+            yield (words, [w2i[x] for x in words], int(tag))
 
+# Read in the data
+train = list(read_dataset("classes/train.txt"))
+w2i = defaultdict(lambda: UNK, w2i)
+dev = list(read_dataset("classes/test.txt"))
+nwords = len(w2i)
+ntags = 5
 
-def read():
-    global w2i, i2w
-    train_path = os.path.join(FLAGS.input_dir, 'classes/train.txt')
-    train = list(read_dataset(train_path))
-    w2i = defaultdict(lambda: UNK, w2i)
-    valid_path = os.path.join(FLAGS.input_dir, 'classes/test.txt')
-    valid = list(read_dataset(valid_path))
-    i2w = {v: k for k, v in w2i.items()}
-
-    return train, valid
+EMB_SIZE = 64
+WIN_SIZE = 3
+FILTER_SIZE = 64
 
 
 def create_model(input):
-    ntags = len(t2i)
-    nwords = len(w2i)
     w_emb = tf.Variable(tf.random_uniform([nwords, 1, 1, EMB_SIZE], -1.0, 1.0), dtype=tf.float32, name='w_emb')
     w_cnn = tf.Variable(tf.random_uniform([1, WIN_SIZE, EMB_SIZE, FILTER_SIZE], -1.0, 1.0), dtype=tf.float32, name='w_cnn')
     b_cnn = tf.Variable(tf.zeros([FILTER_SIZE]), dtype=tf.float32, name='b_cnn')
@@ -63,6 +57,24 @@ def create_loss(score, label):
     sparse = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=label, logits=score)
     loss = tf.reduce_mean(sparse)
     return loss
+
+
+def calc_predict_and_activations(scores):
+    print('%d ||| %s' % (tag, ' '.join(words)))
+    predict = np.argmax(scores)
+    print('scores=%s, predict: %d' % (scores, predict))
+
+
+def display_activations(words, activations):
+    pad_begin = (WIN_SIZE - 1) / 2
+    pad_end = WIN_SIZE - 1 - pad_begin
+    words_padded = ['pad' for i in range(pad_begin)] + words + ['pad' for i in range(pad_end)]
+
+    ngrams = []
+    for act in activations:
+        ngrams.append('[' + ', '.join(words_padded[act:act+WIN_SIZE]) + ']')
+
+    return ngrams
 
 
 def main(_):
@@ -88,6 +100,12 @@ def main(_):
                 iteration_loss += loss_val
             print('iteration: %r; loss: %.4f' % (iter, iteration_loss / len(train)))
 
+        test_correct = 0.0
+        for words, tag in dev:
+            scores = calc_scores(words).npvalue()
+            predict = np.argmax(scores)
+            if predict == tag:
+                test_correct += 1
 
 if __name__ == "__main__":
     tf.app.run()
